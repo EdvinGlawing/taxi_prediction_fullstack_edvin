f2a089da1f9262f582324af8f72a435d34edc52a
EdvinGlawing <glawing.edvin@gmail.com>
2026-01-16 20:28:48 +0100

    Trained and tested model with linear regression and random forest, selected random forest model for the project and exported with joblib

--- src/taxipred/utils/constants.py
+++ src/taxipred/utils/constants.py
@@ -2,3 +2,5 @@ from pathlib import Path
 
 DATA_PATH = Path(__file__).parents[1] / "data"
 TAXI_CSV_PATH = DATA_PATH / "taxi_trip_pricing.csv"
+CLEAN_TAXI_CSV_PATH = DATA_PATH /"cleaned_taxi_data.csv"
+MODEL_PATH = DATA_PATH /"taxi_price_predictor.joblib"
