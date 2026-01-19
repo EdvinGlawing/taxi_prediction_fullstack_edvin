88b3fe5e150e9b589aba1841165100c97c5db113
EdvinGlawing <glawing.edvin@gmail.com>
2026-01-19 10:07:48 +0100

    added categorical features for the ML model and retrained it

--- src/taxipred/backend/data_processing.py
+++ src/taxipred/backend/data_processing.py
@@ -1,11 +1,17 @@
-from taxipred.utils.constants import TAXI_CSV_PATH
+from taxipred.utils.constants import CLEAN_TAXI_CSV_PATH, MODEL_PATH
 import pandas as pd
 import json
+import joblib
 
 class TaxiData:
     def __init__(self):
-        self.df = pd.read_csv(TAXI_CSV_PATH)
+        self.df = pd.read_csv(CLEAN_TAXI_CSV_PATH)
+        self.model = joblib.load(MODEL_PATH)
 
     def to_json(self):
         return json.loads(self.df.to_json(orient="records"))
         
+    def predict_price(self, input_data: dict) -> float:
+        input_df = pd.DataFrame([input_data])
+        prediction = self.model.predict(input_df)[0]
+        return float(prediction)
