8f41854e502d9850b5af172fadf4b8bd312af408
EdvinGlawing <glawing.edvin@gmail.com>
2026-01-21 11:24:51 +0100

    user input and prediction endpoint added

--- src/taxipred/backend/api.py
+++ src/taxipred/backend/api.py
@@ -1,11 +1,27 @@
 from fastapi import FastAPI
 from taxipred.backend.data_processing import TaxiData
+from pydantic import BaseModel, Field
+
 
 app = FastAPI()
 
 taxi_data = TaxiData()
 
+class TaxiUserInput(BaseModel):
+    Trip_Distance_km: float = Field(gt=0)
+    Passenger_Count: int = Field(ge=1, le=4)
+    Weather: str
+    Traffic_Conditions: str
+    Day_of_Week: str
+    Time_of_Day: str
 
 @app.get("/")
 def health_check():
-    return {"status": "ok"}
+    return {"status": "API running"}
+
+@app.post("/predict")
+def predict_price(trip: TaxiUserInput):
+    return {
+        "message": "Prediction endpoint running",
+        "input_received": trip.model_dump()
+    }
