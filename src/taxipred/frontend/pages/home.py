32cfdd1a76a09dcaba28ff63f449612131849815
EdvinGlawing <glawing.edvin@gmail.com>
2026-01-22 12:50:55 +0100

    streamlit homepage picture added, readme updated with images and short info about what the repo contains

--- src/taxipred/frontend/pages/home.py
+++ src/taxipred/frontend/pages/home.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from taxipred.utils.constants import IMAGE_PATH
 
 st.markdown("# Edvins Taxi Price Predictor")
 
@@ -6,3 +7,5 @@ st.markdown(""" This is a streamlit app that uses a ML model to calculate taxi t
 To use this application, press Taxi Price Predictor on the sidebar and input your trip details.
 
 """)
+
+st.image(IMAGE_PATH/"taxipredpic.png") # AI generated picture
